97761796c850fa971a844acccc2c9a237526a36e
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2022-11-16 03:41:31 +0530

    Update eks-data.sh

--- eks-data.sh
+++ eks-data.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 vpc=$(sed -n 4p zx.sh | cut -c 10- )
-pub=$(sed -n 2p zx.sh | cut -c 21- | sed 's/.........................$//' | sed  's/.$/"/')
-pri=$(sed -n 1p zx.sh | cut -c 22- | sed 's/.........................$//' |sed  's/.$/"/')
-pub1=$(sed -n 2p zx.sh | cut -c 22- | sed 's/..........................$//')
-pub2=$(sed -n 2p zx.sh | cut -c 22- | sed 's/..........................$//')
+pub1=$(sed -n 2p zx.sh | cut -c 21- | sed 's/.........................$//' | sed  's/.$/"/')
+pub2=$(sed -n 2p zx.sh | cut -c 47-  |sed 's/^/"/')
+pri1=$(sed -n 1p zx.sh | cut -c 22- | sed 's/.........................$//' |sed  's/.$/"/')
+pri2=$(sed -n 1p zx.sh | cut -c 48-  |sed 's/^/"/')
+quotepub=$(sed -n 2p zx.sh | cut -c 22- | sed 's/..........................$//')
+quotepri=$(sed -n 1p zx.sh | cut -c 23- | sed 's/..........................$//')
 sed -i "10s/.*/  id: $vpc/" eks.yaml
-sed -i "14s/.*/        id: $pub/" eks.yaml
-sed -i "17s/.*/        id: $pub/" eks.yaml
-sed -i "21s/.*/        id: $pri/" eks.yaml
-sed -i "24s/.*/        id: $pri/" eks.yaml
+sed -i "14s/.*/        id: $pub1/" eks.yaml
+sed -i "17s/.*/        id: $pub2/" eks.yaml
+sed -i "21s/.*/        id: $pri1/" eks.yaml
+sed -i "24s/.*/        id: $pri2/" eks.yaml
 
-sed -i "81s/.*/      - $pub1/" eks.yaml
-sed -i "94s/.*/      - $pub2/" eks.yaml
+sed -i "94s/.*/      - $quotepub/" eks.yaml
+sed -i "81s/.*/      - $quotepri/" eks.yaml
